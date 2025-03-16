ee9c502e867a9be3a01568dcc47c9e0b8f7fe69f
Rodrigo Martín Cerón <130743189+Matorry@users.noreply.github.com>
2025-03-16 11:50:32 +0100

    Update users.repository.ts

--- src/repository/users.repository.ts
+++ src/repository/users.repository.ts
@@ -3,27 +3,13 @@ import { Contact } from '../types/contact.js';
 
 export class UsersRepository {
   async contact(data: Contact) {
-    const { name, email, phone, isOkTerms, comments } = data;
-
+    const { name, email, message } = data;
+    
     await transporter.sendMail({
-      from: `WeRent ${process.env.EMAIL}`,
-      to: email,
-      subject: 'Solicitud de contacto recibida',
-      text: `Hola ${name}, esto es una prueba.`,
-      html: `
-        <p>Nombre: ${name}</p>
-        <p>email: ${email}</p>
-        <p>telefono: ${phone}</p>
-        <p>terminos y condiciones: ${isOkTerms}</p>
-        <p>comments: ${comments}</p>
-        <span>WeRent</span>`,
-    });
-
-    await transporter.sendMail({
-      from: `WeRent-Contact-Center ${process.env.EMAIL}`,
+      from: `${process.env.EMAIL}`,
       to: `${process.env.EMAIL}`,
-      subject: 'Solicitud de contacto',
-      text: ` Nombre: ${name}, email: ${email}, telefono: ${phone},  terminos y condiciones: ${isOkTerms}, comments: ${comments}.`,
+      subject: 'Solicitud de contacto portfolio',
+      text: ` Nombre: ${name}, email: ${email}, message: ${message}.`,
     });
   }
 }
